90826162543e5ce16cd6260b8a7ae8e4edb79f56
NICOLÁS BERRÍOS <neberrios@uc.com>
2017-11-16 16:07:53 -0300

    trash

---
+++ EatForHonor!/Assets/Scripts/counter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class counter : MonoBehaviour {
+
+    private int honor = 0;
+    private int deshonra = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy"){
+            if (collision.gameObject.GetComponent<life>().health == 0){
+                honor++;
+                Debug.Log("honor");
+            }else{
+                deshonra++;
+                Debug.Log("deshonra");
+            }
+            Destroy (collision.gameObject);
+        }
+        
+    }
+}
---
+++ EatForHonor!/Assets/Scripts/life.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class life : MonoBehaviour {
+
+    public int health = 6;    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //perder vida
+}
