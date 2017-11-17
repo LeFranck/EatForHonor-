b988a5aeb2330d2f6f09c8f3afea3a3038a6a0f5
NICOLÁS BERRÍOS <neberrios@uc.com>
2017-11-17 03:12:20 -0300

    boton silla funciona

---
+++ EatForHonor!/Assets/Scripts/CreateButtons.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateButtons : MonoBehaviour
+{
+
+    public GameObject man;
+    public GameObject woman;
+    public GameObject boy;
+    public GameObject girl;
+    public Transform canvas;
+    //public Transform self;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CreateMan()
+    {
+        Instantiate(man, new Vector3(transform.position.x + transform.localScale.x / 2 + man.transform.localScale.x / 2, transform.position.y + transform.localScale.y / 2 + man.transform.localScale.y / 2, 0), Quaternion.identity, canvas);
+    }
+
+    public void CreateWoman()
+    {
+        Instantiate(woman, new Vector3(transform.position.x - transform.localScale.x / 2 - woman.transform.localScale.x / 2, transform.position.y + transform.localScale.y / 2 + woman.transform.localScale.y / 2, 0), Quaternion.identity, canvas);
+    }
+
+    public void CreateBoy()
+    {
+        Instantiate(boy, new Vector3(transform.position.x + transform.localScale.x / 2 + boy.transform.localScale.x / 2, transform.position.y - transform.localScale.y / 2 - boy.transform.localScale.y / 2, 0), Quaternion.identity, canvas);
+    }
+
+    public void CreateGirl()
+    {
+        Instantiate(girl, new Vector3(transform.position.x - transform.localScale.x / 2 - girl.transform.localScale.x / 2, transform.position.y - transform.localScale.y / 2 - girl.transform.localScale.y / 2, 0), Quaternion.identity, canvas);
+    }
+
+}
