f533b3d1232d4661608de12f673b09dcb5803279
NICOLÁS BERRÍOS <neberrios@uc.com>
2017-11-17 00:51:46 -0300

    script y avances y weas luego de pushear a dev

---
+++ EatForHonor!/Assets/CreateButtons.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateButtons : MonoBehaviour {
+
+    public GameObject man;
+    public GameObject woman;
+    public GameObject boy;
+    public GameObject girl;
+    
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void CreateMan() {
+        Instantiate(man, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    public void CreateWoman()
+    {
+        Instantiate(woman, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    public void CreateBoy()
+    {
+        Instantiate(boy, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    public void CreateGirl()
+    {
+        Instantiate(girl, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
