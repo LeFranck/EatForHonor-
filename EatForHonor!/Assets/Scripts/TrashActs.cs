996f04e0aa23af6d8fc401730166b9795726688e
LeFranck <fralfarouc@gmail.com>
2017-11-22 18:32:21 -0300

    food spawner vacio

--- EatForHonor!/Assets/Scripts/TrashActs.cs
+++ EatForHonor!/Assets/Scripts/TrashActs.cs
@@ -17,14 +17,15 @@ public class TrashActs : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D collision)
 	{
-		Debug.Log("CHOCOOO");
 		if (collision.gameObject.tag == "Enemy"){
 			if (collision.gameObject.GetComponent<life>().health == 0){
 				GameManager.instance.honor += 1;
+				GameManager.instance.Info.transform.Find("txtHonor").GetComponent<TextMesh>().text = GameManager.instance.honor+"";
 				GameManager.instance.score += 1000;
 				Debug.Log("honor");
 			}else{
 				GameManager.instance.deshonor += 1;
+				GameManager.instance.Info.transform.Find("txtDeshonor").GetComponent<TextMesh>().text = GameManager.instance.deshonor+"";
 				Debug.Log("deshonra");
 			}
 			Destroy (collision.gameObject);
